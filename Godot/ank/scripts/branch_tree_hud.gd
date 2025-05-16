89c0c36cd359543737c488299ba7ff63e25358be
jaygon4146 <129012837+jaydenpm@users.noreply.github.com>
2025-05-16 15:11:12 +1000

    Minor File re-organization + Marker for Cursor that changes texture based on player active tool
    content, scenes and scripts directories
    Created icon_peep.png and icon_branch.png
    NodePlayerState +Marker2DCursor
    node_player_state updates SpriteTool texture based on active tool

--- Godot/ank/scripts/branch_tree_hud.gd
+++ Godot/ank/scripts/branch_tree_hud.gd
@@ -2,4 +2,4 @@ extends CanvasLayer
 
 
 func _on_button_back_to_start_pressed() -> void:
-	get_tree().change_scene_to_file("scene_start.tscn")
+	get_tree().change_scene_to_file("res://scenes/scene_start.tscn")
--- Godot/ank/scripts/node_player_state.gd
+++ Godot/ank/scripts/node_player_state.gd
@@ -3,7 +3,14 @@ extends Node
 enum PlayerTool {TOOL_NONE, TOOL_PEEP, TOOL_BRANCH}
 var player_active_tool = PlayerTool.TOOL_NONE
 
+var sprite_player_tool : Sprite2D
+@export var texture_icon_peep : Texture
+@export var texture_icon_branch : Texture
+
 signal player_tool_changed
+
+func _ready():
+	sprite_player_tool = get_node("Marker2DCursor/SpriteTool")
 
 func _on_button_peep_pressed() -> void:
 	_change_player_active_tool(PlayerTool.TOOL_PEEP)
@@ -20,3 +27,15 @@ func _change_player_active_tool(tool) -> void:
 	#print("_change_player_active_tool: from: ", player_active_tool, "  to: ", tool)
 	player_active_tool = tool
 	player_tool_changed.emit(player_active_tool)
+	
+	var new_texture
+	match player_active_tool:
+		null, PlayerTool.TOOL_NONE:
+			pass
+		PlayerTool.TOOL_PEEP:
+			new_texture = texture_icon_peep
+		PlayerTool.TOOL_BRANCH:
+			new_texture = texture_icon_branch
+	
+	if (sprite_player_tool):
+		sprite_player_tool.set_texture(new_texture)
